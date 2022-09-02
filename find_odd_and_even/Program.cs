b56e5dfe8c797c5a61173f51dc633928d8cf5beb
waytothendless <waytothendless@yandex.ru>
2022-09-02 11:23:32 +0300

    add find_odd_and_even

---
+++ find_odd_and_even/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+if (number%2 == 0){
+    Console.WriteLine("Число четное");
+} else {
+    Console.WriteLine("Число нечетное");
+}
