55ede83333ba92a2d34649c023b945e101936c93
waytothendless <waytothendless@yandex.ru>
2022-09-02 11:35:41 +0300

    add find_all_evens_before

---
+++ find_all_evens_before/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+int i = 1;
+
+while (i <= number) {
+    if (i % 2 == 0) {
+        Console.WriteLine(i);
+    }
+
+    i++;
+}
