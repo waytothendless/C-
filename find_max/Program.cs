dd0a1504bf25ba8aebd0395eb64ae6758fcdd3b7
waytothendless <waytothendless@yandex.ru>
2022-09-02 11:08:39 +0300

    add find_max

---
+++ find_max/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+int max = number1;
+if (number2 > max)
+{
+    max = number2;
+}
+
+Console.WriteLine("maximum is " + max);
