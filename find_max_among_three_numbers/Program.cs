fb95dd9e6599fc80f3c6838e274e425fd8b1745f
waytothendless <waytothendless@yandex.ru>
2022-09-02 11:16:31 +0300

    add find_max_among_three_numbers

---
+++ find_max_among_three_numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int number3 = int.Parse(Console.ReadLine());
+
+int max = number1;
+if (number2 > max) {
+    max = number2;
+}
+if (number3 > max) 
+{
+    max = number3;
+}
+
+Console.Write("maximum is ");
+Console.WriteLine(max);
