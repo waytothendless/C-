ad6d2ca4a647dc75af8e8be321560d2c7aa6f3a6
waytothendless <waytothendless@yandex.ru>
2022-09-15 23:57:59 +0300

    message

---
+++ count_sum_of_digits/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+int number_of_digits = (int)Math.Floor(Math.Log10(number) + 1);
+int sum = 0;
+int current = number;
+int current_digit = 0;
+while(number_of_digits > 0) {
+    current_digit = current % 10;
+    sum = sum + current_digit;
+    current = current / 10;
+    number_of_digits--;
+}
+
+Console.WriteLine(sum);
---
+++ init_array/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите размерность массива:");
+int size = int.Parse(Console.ReadLine());
+int[] nums = new int[size];
+int i=0;
+while(i < size) {
+    Console.WriteLine("Задайте элемент массива:");
+    int element = int.Parse(Console.ReadLine());
+    nums[i] = element;
+    i++;
+}
+
+ Console.WriteLine("[{0}]", string.Join(", ", nums));
