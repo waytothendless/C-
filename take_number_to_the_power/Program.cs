c2db5e0c1b17725918e0b588b1c76129756b38ed
waytothendless <waytothendless@yandex.ru>
2022-09-15 22:25:17 +0300

    take_number_to_degree

---
+++ take_number_to_the_power/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое число:");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите показатель степени:");
+int degree = int.Parse(Console.ReadLine());
+int i =1;
+int result=1;
+while(i<=degree) {
+    result = result*number;
+    i++;
+}
+
+Console.WriteLine(result);
