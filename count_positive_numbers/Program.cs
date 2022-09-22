60bfa6035bddc4b8086af9e6d1a2cbedbe166f07
waytothendless <waytothendless@yandex.ru>
2022-09-22 22:46:52 +0300

    homework

---
+++ count_positive_numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿void count_number_of_positive_numbers(int limit, int count, int num_of_positives) {
+    Console.WriteLine("Введите число:");
+    int number = int.Parse(Console.ReadLine());
+    int number_of_positives = num_of_positives;
+    if (number > 0) {
+        number_of_positives++;
+    }
+    
+    if (count >= limit) {
+        Console.WriteLine("Количестве цифр больше нуля:"+number_of_positives);
+        return;
+    }
+
+    int counter = count;
+    count_number_of_positive_numbers(limit, counter+1, number_of_positives);
+}
+
+Console.WriteLine("Задайте количество чисел, которые желаете ввести:");
+int limit = int.Parse(Console.ReadLine());
+if (limit <=0) {
+    Console.WriteLine("Значение должно быть больше нуля");
+    return;
+}
+int count = 1;
+count_number_of_positive_numbers(limit,count,0);
---
+++ find_cross_point/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Задайте k1");
+int k1=int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте b1");
+int b1=int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте k2");
+int k2=int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте b2");
+int b2=int.Parse(Console.ReadLine());
+Console.WriteLine("Точка пересечения:("+((float)b1*(-1.0)/(float)k1)+";"+((float)b2*(-1.0)/(float)k2)+")");
